e9b08e52bb833d598745e0828fdd042213174a77
frankpalardy <57007code@gmail.com>
2025-01-14 00:56:38 -0600

    add Kafka test

--- src/test/java/org/example/webapp/KafkaProducerServiceTest.java
+++ src/test/java/org/example/webapp/KafkaProducerServiceTest.java
@@ -1,4 +1,29 @@
 package org.example.webapp;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.kafka.core.KafkaTemplate;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
 public class KafkaProducerServiceTest {
+
+    @Mock
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @InjectMocks
+    private KafkaProducerService kafkaProducerService;
+
+    @Test
+    public void testSendMessage() {
+        String message = "test message";
+
+        kafkaProducerService.sendMessage(message);
+
+        verify(kafkaTemplate).send("trades", message);
+    }
 }
